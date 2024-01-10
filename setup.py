3671a3fb2d4f679c07555a9698e4df4eec3311c7
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-01-11 00:34:32 +0100

    feat: add description

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
-import subprocess
-
 import os
 
 visiontagger_version = os.getenv('PACKAGE_VERSION', '0.1.0')
+
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="VisionTagger",
     version=visiontagger_version,
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     package_data={
         "visiontagger": ["*.png"],
