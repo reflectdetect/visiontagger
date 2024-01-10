b27534a9b82aa5bcdcc087e58cacaeb52774879c
Lars Kaesberg <33939116+lkaesberg@users.noreply.github.com>
2024-01-11 00:04:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import subprocess
 
 visiontagger_version = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
 assert "." in visiontagger_version
